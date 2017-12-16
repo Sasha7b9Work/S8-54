9c09ab8cbc9329b83313238c01e36a49a5733aeb
Sasha <Sasha7b9@tut.by>
2017-12-16 22:38:35 +0300

    *

--- sources/libs/common/_VS/stdlib.h
+++ sources/libs/common/_VS/stdlib.h
@@ -1,8 +1,10 @@
 #pragma once
 
-void memcpy(void *, const void *, int);
-void memset(void *, int, int);
 void *malloc(int);
 void free(void *);
-int memcmp(void *, const void *, int);
 #define RAND_MAX 0x7fffffff
+
+
+#define __disable_irq()
+#define __set_MSP(x)
+#define __enable_irq()
--- sources/libs/common/_VS/string.h
+++ sources/libs/common/_VS/string.h
@@ -8,3 +8,7 @@ int   strcmp(char *, char *);
 char  toupper(char);
 char  tolower(char);
 float atof(char *);
+void  memcpy(void *, const void *, int);
+void  memset(void *, int, int);
+int   memcmp(void *, const void *, int);
+
