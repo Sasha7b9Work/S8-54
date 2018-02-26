3f8c518c17aea895a3c7ea19b5cb37bc63aebadd
Sasha7b9 <Sasha7b9@tut.by>
2018-02-26 11:03:30 +0300

    Рефакторинг

--- sources/Loader54/src/Hardware/FSMC.cpp
+++ sources/Loader54/src/Hardware/FSMC.cpp
@@ -10,10 +10,10 @@
 #endif
 
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-static ModeFSMC curMode = ModeFSMC_None;
-static ModeFSMC prevMode = ModeFSMC_None;
-static bool inSetStateMode = false;
-static pFuncBV funcAfterSetMode = 0;
+//static ModeFSMC curMode = ModeFSMC_None;
+//static ModeFSMC prevMode = ModeFSMC_None;
+//static bool inSetStateMode = false;
+//static pFuncBV funcAfterSetMode = 0;
 
 
 static SRAM_HandleTypeDef gSramHandle =
@@ -45,137 +45,29 @@ static SRAM_HandleTypeDef gSramHandle =
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 void FSMC::Init()
 {
-    curMode = ModeFSMC_None;
-    prevMode = ModeFSMC_None;
+//    curMode = ModeFSMC_None;
+//    prevMode = ModeFSMC_None;
 }
 
 
 //----------------------------------------------------------------------------------------------------------------------------------------------------
-void FSMC::SetMode(ModeFSMC mode)
+void FSMC::SetMode(ModeFSMC)
 {
-    if (mode != curMode)
+    static const FMC_NORSRAM_TimingTypeDef sramTiming =
     {
-        inSetStateMode = true;
-
-        prevMode = curMode;
-
-        curMode = mode;
-
-        if (curMode == ModeFSMC_Display)
-        {
-            /*
-            static const FMC_NORSRAM_TimingTypeDef sramTiming =
-            {
-                5,                  // FSMC_AddressSetupTime
-                7,                  // FSMC_AddressHoldTime
-                13,                 // FSMC_DataSetupTime
-                11,                 // FSMC_BusTurnAroundDuration
-                3,                  // FSMC_CLKDivision
-                0,                  // FSMC_DataLatency
-                FMC_ACCESS_MODE_B   // FSMC_AccessMode
-            };
-            */
-
-            /// \todo временно изменил - возможно, на флешку не пишет из-за неправильных таймингов
-            static const FMC_NORSRAM_TimingTypeDef sramTiming =
-            {
-                6,                  // FSMC_AddressSetupTime
-                8,                  // FSMC_AddressHoldTime
-                14,                 // FSMC_DataSetupTime
-                12,                 // FSMC_BusTurnAroundDuration
-                3,                  // FSMC_CLKDivision
-                0,                  // FSMC_DataLatency
-                FMC_ACCESS_MODE_B   // FSMC_AccessMode
-            };
-
-            static const SRAM_HandleTypeDef sramHandle =
-            {
-                FMC_NORSRAM_DEVICE,
-                FMC_NORSRAM_EXTENDED_DEVICE,
-                {
-                    FMC_NORSRAM_BANK1,                 // Init.NSBank
-                    FMC_DATA_ADDRESS_MUX_ENABLE,       // Init.DataAddressMux
-                    FMC_MEMORY_TYPE_NOR,               // Init.MemoryType
-                    FMC_NORSRAM_MEM_BUS_WIDTH_8,       // Init.MemoryDataWidth
-                    FMC_BURST_ACCESS_MODE_DISABLE,     // Init.BurstAccessMode
-                    FMC_WAIT_SIGNAL_POLARITY_LOW,      // Init.WaitSignalPolarity
-#ifdef STM32F437xx
-                    FMC_WRAP_MODE_DISABLE,             // Init.WrapMode
-#endif
-                    FMC_WAIT_TIMING_BEFORE_WS,         // Init.WaitSignalActive
-                    FMC_WRITE_OPERATION_ENABLE,        // Init.WriteOperation
-                    FMC_WAIT_SIGNAL_DISABLE,           // Init.WaitSignal
-                    FMC_EXTENDED_MODE_DISABLE,         // Init.ExtendedMode
-                    FMC_ASYNCHRONOUS_WAIT_DISABLE,     // Init.AsynchronousWait
-                    FMC_WRITE_BURST_DISABLE,           // Init.WriteBurst
-                    0, 0, 0
-                },
-                HAL_UNLOCKED, HAL_SRAM_STATE_RESET, 0
-            };
-
-            if (HAL_SRAM_Init((SRAM_HandleTypeDef*)(&sramHandle), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming)) != HAL_OK)
-            {
-            };
-        }
-        else if (curMode == ModeFSMC_FPGA)
-        {
-            static const FMC_NORSRAM_TimingTypeDef sramTiming =
-            {
-                2,                  // FSMC_AddressSetupTime
-                4,                  // FSMC_AddressHoldTime
-                9,                  // FSMC_DataSetupTime   При значении 9 32кБ записываются в RAM за 1000мкс. Уменьшение
-                                    // на одну единцу уменьшает этот параметр на 90 мкс. Если 3 - 32кБ запишутся за 460 мкс.
-                0,                  // FSMC_BusTurnAroundDuration
-                0,                  // FSMC_CLKDivision
-                0,                  // FSMC_DataLatency
-                FMC_ACCESS_MODE_C   // FSMC_AccessMode
-            };
-
-            if (HAL_SRAM_Init((SRAM_HandleTypeDef*)(&gSramHandle), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming)) != HAL_OK)
-            {
-            };
-        }
-        else if (curMode == ModeFSMC_RAM)
-        {
-            static const FMC_NORSRAM_TimingTypeDef sramTimingWrite =
-            {
-                1,                  // FSMC_AddressSetupTime    0...15
-                1,                  // FSMC_AddressHoldTime     1...15
-                7,                  // FSMC_DataSetupTime       1...255
-                0,                  // FSMC_BusTurnAroundDuration   0...15
-                0,                  // FSMC_CLKDivision         not using
-                0,                  // FSMC_DataLatency         not using
-                FMC_ACCESS_MODE_C   // FSMC_AccessMode
-            };
-
-            static const FMC_NORSRAM_TimingTypeDef sramTimingRead =
-            {
-                1,                  // FSMC_AddressSetupTime    
-                1,                  // FSMC_AddressHoldTime
-                1,                  // FSMC_DataSetupTime
-                0,                  // FSMC_BusTurnAroundDuration
-                0,                  // FSMC_CLKDivision
-                0,                  // FSMC_DataLatency
-                FMC_ACCESS_MODE_C   // FSMC_AccessMode
-            };
-
-            //HAL_SRAM_DeInit((SRAM_HandleTypeDef*)(&gSramHandle));
-
-            if (HAL_SRAM_Init((SRAM_HandleTypeDef*)(&gSramHandle), (FMC_NORSRAM_TimingTypeDef*)(&sramTimingWrite), (FMC_NORSRAM_TimingTypeDef*)(&sramTimingRead)) != HAL_OK)
-            {
-            };
-        }
-
-        inSetStateMode = false;
-
-        if (funcAfterSetMode)
-        {
-            if(funcAfterSetMode())
-            {
-                funcAfterSetMode = 0;
-            }
-        }
-    }
+        2,                  // FSMC_AddressSetupTime
+        4,                  // FSMC_AddressHoldTime
+        9,                  // FSMC_DataSetupTime   При значении 9 32кБ записываются в RAM за 1000мкс. Уменьшение
+                            // на одну единцу уменьшает этот параметр на 90 мкс. Если 3 - 32кБ запишутся за 460 мкс.
+        0,                  // FSMC_BusTurnAroundDuration
+        0,                  // FSMC_CLKDivision
+        0,                  // FSMC_DataLatency
+        FMC_ACCESS_MODE_C   // FSMC_AccessMode
+    };
+    
+    if (HAL_SRAM_Init((SRAM_HandleTypeDef*)(&gSramHandle), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming), (FMC_NORSRAM_TimingTypeDef*)(&sramTiming)) != HAL_OK)
+    {
+    };
 }
 
 #ifdef WIN64
