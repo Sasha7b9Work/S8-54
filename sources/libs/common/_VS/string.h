0faec743db7781c80b234e8d43b91cd2a295e46c
Sasha7b9 <Sasha7b9@tut.by>
2018-02-28 15:46:15 +0300

    *

--- sources/libs/common/_VS/string.h
+++ sources/libs/common/_VS/string.h
@@ -3,12 +3,12 @@
 
 char   *strcat(char *, const char *);
 char   *strcpy(char *, const char *);
-size_t  strlen(const char *);
+unsigned int  strlen(const char *);
 int     strcmp(char *, char *);
 char    toupper(char);
 char    tolower(char);
 float   atof(char *);
-void    memcpy(void *, const void *, size_t);
+void    memcpy(void *, const void *, unsigned int);
 void    memset(void *, int, unsigned int);
 int     memcmp(void *, const void *, int);
 
