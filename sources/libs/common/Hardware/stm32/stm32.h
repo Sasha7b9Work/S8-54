a46d913320ac779b77cfd077de7bb86cc9ebafcb
Sasha7b9 <Sasha7b9@tut.by>
2018-03-01 12:14:05 +0300

    Рефакторинг

--- sources/libs/common/Hardware/stm32/stm32.h
+++ sources/libs/common/Hardware/stm32/stm32.h
@@ -10,21 +10,3 @@ public:
 
 protected:
 };
-
-#ifdef __cplusplus
-extern "C" {
-#endif
-
-void NMI_Handler();
-void HardFault_Handler();
-void MemManage_Handler();
-void BusFault_Handler();
-void UsageFault_Handler();
-void SVC_Handler();
-void DebugMon_Handler();
-void PendSV_Handler();
-void SysTick_Handler();
-
-#ifdef __cplusplus
-}
-#endif
--- sources/libs/common/Hardware/stm32/stm32_it.cpp
+++ sources/libs/common/Hardware/stm32/stm32_it.cpp
@@ -12,6 +12,17 @@
 extern "C" {
 #endif
 
+void NMI_Handler();
+void HardFault_Handler();
+void MemManage_Handler();
+void BusFault_Handler();
+void UsageFault_Handler();
+void SVC_Handler();
+void DebugMon_Handler();
+void PendSV_Handler();
+void SysTick_Handler();
+
+
 //----------------------------------------------------------------------------------------------------------------------------------------------------
 __attribute((noreturn)) void HardFault_Handler(void)
 {
