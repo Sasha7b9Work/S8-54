5551fd16a692ef75db0c21683e30a479dc51cb00
Sasha7b9 <Sasha7b9@tut.by>
2018-03-02 13:42:17 +0300

    Рефакторинг

--- sources/Loader54/src/Hardware/CPU.h
+++ sources/Loader54/src/Hardware/CPU.h
@@ -139,7 +139,18 @@ private:
 extern "C" {
 #endif
 
+    /// Прерывание на таймер
     void TIM3_IRQHandler();
+
+    void NMI_Handler();
+    void HardFault_Handler();
+    void MemManage_Handler();
+    void BusFault_Handler();
+    void UsageFault_Handler();
+    void SVC_Handler();
+    void DebugMon_Handler();
+    void PendSV_Handler();
+    void SysTick_Handler();
 
 #ifdef __cplusplus
 }
