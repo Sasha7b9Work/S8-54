22f2a7668dd73f2536c202724e84d3b9cef0277e
Sasha7b9 <Sasha7b9@tut.by>
2021-02-10 08:31:48 +0300

    Удалена информация о телефонах из стартового экрана

--- sources/Device54/src/defines.h
+++ sources/Device54/src/defines.h
@@ -17,7 +17,10 @@
 // В случае С8-54 заккоментировать следующую строку. В случае C8-55 раскомментировать строку
 //#define S8_55
 
-#if defined S8_54
+#if defined S8_55
+#define MODEL_RU "С8-55"
+#define MODEL_EN "S8-55"
+#else
 #define MODEL_RU "С8-54"
 #define MODEL_EN "S8-54"
 #endif
