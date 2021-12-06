5a42ec47b7ef9d8b14e8bf01af974e23a1cb58db
Sasha7b9 <Sasha7b9@tut.by>
2021-12-06 21:56:01 +0300

    *

--- sources/Device54/src/SCPI/SCPI.h
+++ sources/Device54/src/SCPI/SCPI.h
@@ -1,11 +1,6 @@
 #pragma once
 #include "Ethernet/TcpSocket.h"
 
-/** @defgroup SCPI
- *  @{
- */
-
-
 
 #define ENTER_ANALYSIS                                      \
     Word parameter;                                         \
@@ -65,6 +60,3 @@ void Process_DISPLAY(uint8 *buffer);
 void Process_CHANNEL(uint8 *buffer);
 void Process_TRIG(uint8 *buffer);
 void Process_TBASE(uint8 *buffer);
-
-/** @}
- */
