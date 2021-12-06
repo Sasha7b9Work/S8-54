e54c2802090adaaf2d90d151861b79394fb05787
Sasha7b9 <Sasha7b9@tut.by>
2021-12-06 14:58:00 +0300

    *

--- sources/Device54/src/main.cpp
+++ sources/Device54/src/main.cpp
@@ -33,8 +33,6 @@ int main()
     LAN::Init();
     Menu::Init();
 
-    CONSOLE_NUM_STRINGS = 20;
-
     while(1)
     {
         Timer::StartMultiMeasurement();  // —брос таймера дл€ замера длительности временных интервалов в течение одной
