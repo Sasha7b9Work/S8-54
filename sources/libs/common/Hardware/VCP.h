db8efa15569ff4f13335e8169dc17e193b21e0e1
Sasha7b9 <Sasha7b9@tut.by>
2021-12-07 13:26:44 +0300

    *

--- sources/libs/common/Hardware/VCP.h
+++ sources/libs/common/Hardware/VCP.h
@@ -10,7 +10,7 @@ public:
     /// Эта строка передаётся с завершающими символами \\r\\n
     static void SendStringAsynch(char *format, ...);
 
-    static void SendBufferSynch(const unsigned char *data, int size);
+    static void SendBufferSynch(const uint8 *data, int size);
 
     static void Flush();
 };
