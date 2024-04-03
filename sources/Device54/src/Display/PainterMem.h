cfb0cc849a9538dc2ec85d29da15ad28cbe6db5b
Sasha7b9Work <Phuntik-it@tut.by>
2024-04-03 08:25:25 +0300

    refact

--- sources/Device54/src/Display/PainterMem.h
+++ sources/Device54/src/Display/PainterMem.h
@@ -2,31 +2,17 @@
 #include "defines.h"
 
 
-
-
-/** @addtogroup Display
- *  @{
- *  @addtogroup Painter
- *  @{
- *  @defgroup PainterMem MemoryPainter
- *  @brief Набор функций для рисования в памяти 4-битным цветом
- *  @{
- */
-
-class PainterMem
+namespace PainterMem
 {
-    static void SetBuffer(uint8 *address, int width, int height);
-    
-    static void FillRect(int x, int y, int width, int height, int color);
-    
-    static void SetPoint(int x, int y, int color);
-    
-    static void DrawVLine(int x, int y0, int y1, int color);
-    
-    static void DrawHLine(int y, int x0, int x1, int color);
-    
-    static void DrawRectangle(int x, int y, int widht, int height, int color);
-};
+    void SetBuffer(uint8 *address, int width, int height);
 
-/** @}  @}  @}
- */
+    void FillRect(int x, int y, int width, int height, int color);
+
+    void SetPoint(int x, int y, int color);
+
+    void DrawVLine(int x, int y0, int y1, int color);
+
+    void DrawHLine(int y, int x0, int x1, int color);
+
+    void DrawRectangle(int x, int y, int widht, int height, int color);
+};
